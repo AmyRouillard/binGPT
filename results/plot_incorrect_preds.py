539495bda25d1341213d6cb2e05710fbc83bd20c
AmyRouillard <rouillardamy@gmail.com>
2025-04-04 11:03:27 +0200

    daily

--- results/plot_incorrect_preds.py
+++ results/plot_incorrect_preds.py
@@ -3,6 +3,10 @@
 import numpy as np
 
 data = np.load("./incorrect_preds.npy")
+data1 = np.load("./train incorrect_preds.npy")
+
+print(len(data) + len(data1), f"{2**22:.2e}")
+print(f"{(len(data)+len(data1))/2**22:.2e}")
 
 # %%
 
@@ -11,6 +15,11 @@ y = [d[2] for d in data]
 y_hat = [d[1] for d in data]
 
 diff = [np.sum(np.abs(t - p)) for t, p in zip(y, y_hat)]
+
+# counts of values in diff
+counts = np.bincount(diff)
+print(counts)
+
 
 f = lambda x: [np.sum([2 ** (-i - 1) * d[i] for i in range(len(d))]) for d in x]
 
@@ -32,8 +41,10 @@ for i in range(q - 1):
 import matplotlib.pyplot as plt
 
 fig, ax = plt.subplots(figsize=(10, 5))
-for l, t, p in zip(x, y, y_hat):
-    ax.plot([l, l], [t, p], "-")
+for l, t, p, d in zip(x, y, y_hat, diff):
+    ax.plot([l, l], [t, p], "k-")
+    # add diff as text
+    ax.text(l, (t + p) / 2, str(d), fontsize=18)
 ax.plot(x, y_hat, "o", label="Predicted")
 ax.plot(X, Y, "-", label="Tent map")
 
