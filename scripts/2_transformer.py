e189c66d7d1a32e4048ee719dc79c7d40241d169
AmyRouillard <rouillardamy@gmail.com>
2025-05-26 17:18:29 +0200

    gpu machine

--- scripts/2_transformer.py
+++ scripts/2_transformer.py
@@ -18,7 +18,7 @@ import csv
 dt = time.strftime("%Y_%m_%d_%H_%M", time.localtime())
 
 
-wdir = "C:/Users/Amy/Desktop/Green_Git/binGPT/"
+wdir = "/home/amyrouillard/project-files/" #"C:/Users/Amy/Desktop/Green_Git/binGPT/"
 model_dir = wdir + f"models/{dt}/"  #
 # model_dir = wdir + "models/binary_2025_04_23_13_02"
 
