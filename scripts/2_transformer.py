959360a75650a5d7beaa475d6e66f05b466b6018
AmyRouillard <rouillardamy@gmail.com>
2025-06-02 14:20:57 +0200

    smaller emb

--- scripts/2_transformer.py
+++ scripts/2_transformer.py
@@ -31,7 +31,7 @@ else:
     configs = {
         "data_type": "binary",
         # "data_type": "decimal",
-        "n": 3,
+        "n": 2,
         "length": 18,
     }
 
@@ -89,7 +89,7 @@ else:
     model_config_dict = {
         "n_layer": 8,
         "n_head": 8,
-        "n_embd": 1 * 8,
+        "n_embd": 2 * 8,
         "model_type": None,
         "vocab_size": train_dataset.get_vocab_size(),
         "block_size": train_dataset.get_block_size(),
@@ -137,7 +137,7 @@ if os.path.exists(os.path.join(model_dir, "trainer_config.json")):
 else:
 
     train_config = Trainer.get_default_config()
-    train_config.learning_rate = 3e-2
+    train_config.learning_rate = 1e-2
     train_config.batch_size = 2**13  # 2**15
     train_config.val_batch_size = 2**13  # 2**15
     train_config.max_iters = (
